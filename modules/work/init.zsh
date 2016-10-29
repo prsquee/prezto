6e666e362ed4e822cc3ebd7e457a0aa507f3d69d
prsquee <github@fnord.com.ar>
2016-10-29 16:28:46 -0300

    work stuff2.

---
+++ modules/work/init.zsh
@@ -0,0 +1,17 @@
+# my redhat stuff
+#
+
+# goto fubar
+
+fubar () {
+  if [[ $1 =~ "^[0-9]{8}$" ]]; then
+    ssh -t fubar "cd /fubar/$1 ; bash --login"
+  elif [[ $1 =~ "^/fubar/[0-9]{8}" ]]; then
+    ssh -t fubar "cd $1 ; bash --login"
+  else
+    printf "$0 needs a case number.\n"
+  fi
+}
+
+
+
