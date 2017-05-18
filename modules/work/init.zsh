1a94ea4be19a08c6b49e7b494015e6345687e758
prsquee <github@fnord.com.ar>
2017-05-18 17:08:27 -0300

    zsh login

--- modules/work/init.zsh
+++ modules/work/init.zsh
@@ -5,9 +5,9 @@
 
 fu () {
   if [[ $1 =~ "^[0-9]{8}$" ]]; then
-    ssh -t fubar "cd /fubar/$1 ; bash --login"
+    ssh fubar "cd /fubar/$1; zsh --login"
   elif [[ $1 =~ "^/fubar/[0-9]{8}" ]]; then
-    ssh -t fubar "cd $1 ; bash --login"
+    ssh fubar "cd $1; zsh --login"
   else
     printf "$0 needs a case number.\n"
   fi
