dc5203cfd333184068b957747dad6a7766c4c6b9
sQuEE <squee@Magic-Laptop.local>
2020-10-16 11:23:25 -0300

    export numbers with chpwd

--- modules/work/init.zsh
+++ modules/work/init.zsh
@@ -1,11 +1,19 @@
-# my redhat stuff
-# goto fubar
-
+# my work stuff
+zmodload zsh/pcre
 att() {
   if [[ $1 =~ "^[0-9]{8}$" ]]; then
     attachmentmonster $1
     cd ~/cases/$1
   else
     printf "$0 needs a case number.\n"
+  fi
+}
+
+function chpwd() {
+  if [[ "$PWD" -pcre-match '/(0\d{7})' ]]; then
+     CASE=$match[1]
+     export CASE
+   else
+     unset CASE
   fi
 }
