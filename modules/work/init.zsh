24654b001856ea0b822e376ddc347e9d34179ac1
sQuEE <squee@Magic-Laptop.local>
2020-10-16 11:23:25 -0300

    chpwd

--- modules/work/init.zsh
+++ modules/work/init.zsh
@@ -1,7 +1,18 @@
-# my redhat stuff
-att () {
+# my work stuff
+zmodload zsh/pcre
+
+att() {
   if [[ $1 =~ "^[0-9]{8}$" ]]; then
     set -i
     /home/squee/.local/bin/attachmentmonster $1 ; cd /storage/cases/$1
+  fi
+}
+
+function chpwd() {
+  if [[ "$PWD" -pcre-match '/(0\d{7})' ]]; then
+     CASE=$match[1]
+     export CASE
+   else
+     unset CASE
   fi
 }
