8ab4df7cb78b5dfc70140e5cb2e5aaba96aaa1c5
sQuEE <squee@fnord.com.ar>
2024-06-05 17:13:17 -0300

    add base back

--- modules/work/init.zsh
+++ modules/work/init.zsh
@@ -5,16 +5,17 @@ att() {
   if [[ $1 =~ "^[0-9]{8}$" ]]; then
     set -i
     /usr/bin/casegrab $1 && cd /storage/cases/$1
-  else 
+  else
     /usr/bin/casegrab $CASE && cd /storage/cases/$CASE
   fi
 }
 
 function chpwd() {
-  if [[ "$PWD" -pcre-match '/(0\d{7})' ]]; then
-     CASE=$match[1]
-     export CASE
+  if [[ "$PWD" -pcre-match '(/storage/cases/(\d{8}))' ]]; then
+     BASE=$match[1]
+     CASE=$match[2]
+     export CASE BASE
    else
-     unset CASE
+     unset CASE BASE
   fi
 }
